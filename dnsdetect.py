b7e8b56f8f7e7954d32cf6c13f41f148aa051d34
pponakala <pponakala@cs.stonybrook.edu>
2017-12-08 23:58:15 -0500

    minor

--- dnsdetect.py
+++ dnsdetect.py
@@ -10,7 +10,7 @@ def print_poisoning_details(prev_packet, pkt):
 	print "Answer2 %s" % pkt[DNSRR].rdata
 
 def check_duplicate_responses(pkt):
-	print "checking duplicate packets"
+	#print "checking duplicate packets"
 	for prev_packet in prev_packets:
 		if prev_packet[IP].dst != pkt[IP].dst or\
 		prev_packet[IP].sport != pkt[IP].sport or\
@@ -24,18 +24,18 @@ def check_duplicate_responses(pkt):
 
 def arg_parser():
 	parser = argparse.ArgumentParser(add_help=False)
-	parser.add_argument("-i", default="eth0", help ="interface")
+	parser.add_argument("-i", help ="interface")
 	parser.add_argument("-r", help="tracefile in tcpdump format")
 	parser.add_argument("expression", type=str, help="expression is BPF filter", default="", nargs="?")
 	return parser.parse_args()
 
 def callback(pkt):
 	if not pkt.haslayer(DNS) or not pkt.haslayer(DNSRR):
-		print "no dns,dns rr layer"
+		#print "no dns,dns rr layer"
 		return
 	elif len(prev_packets) < 0:
 		#not received any packets yet to compare
-		print "prev packets queue empty"
+		#print "prev packets queue empty"
 		return
 
 	#parse packet and check with prev unsent packets that were stored
@@ -54,11 +54,16 @@ if __name__ == '__main__':
 	length = 100 #arbitrary
 	prev_packets = deque(maxlen = length)
 
+	print interface
+	print tracefile
+	print expression
+
 	if interface is not None and tracefile is not None:
 		print "Expected: either interface or tracefile. Got: both arguments"
 	elif interface is not None:
 		sniff(filter=bpf_filter, prn=callback, iface=interface, store=0)
 	elif tracefile is not None:
 		sniff(filter=bpf_filter, prn=callback, offline=tracefile, store=0)
-	else:
+	elif interface is None and tracefile is None:
+		interface = 'eth0'
 		sniff(filter=expression, prn=callback, store=0)
